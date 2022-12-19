98326fb971884e45a8d7eccf881ac0eeac0ade78
MinJunJA <110808017+MinJunJA@users.noreply.github.com>
2022-12-19 12:16:48 +0900

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -84,20 +84,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default':{
-        'ENGINE' : 'django.db.backends.mysql',
-        'NAME' : 'snack',
-        'USER' : 'root',
-        'PASSWORD' : 'Alswns0524@',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
-    }
-}
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
+#     'default':{
+#         'ENGINE' : 'django.db.backends.mysql',
+#         'NAME' : 'snack',
+#         'USER' : 'root',
+#         'PASSWORD' : 'Alswns0524@',
+#         'HOST' : 'localhost',
+#         'PORT' : '3306',
+#     }
+# }
 
 
 # Password validation
@@ -171,4 +171,3 @@ DATABASES = {
         'OPTIONS':{
             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
         }
-
